fbf369a1baad909ab9ddd7c0259c798cf3c30f32
rohailiam <mrohail2928@gmail.com>
2023-06-01 17:37:01 +0500

    res added

--- index.js
+++ index.js
@@ -1,159 +1,144 @@
-import fetch from 'node-fetch';
-import cheerio from 'cheerio'
+import fetch from "node-fetch";
+import cheerio from "cheerio";
 // let url = "https://www.tiktok.com/@okx/video/7223113390380698886"
 
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('<marquee>Running...</marquee>')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("<marquee>Running...</marquee>");
+});
 
-app.post('/youtube', async (req, res) => {
+app.post("/youtube", async (req, res) => {
   const { url } = req.query;
   let data = fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.text();
-    }
-    console.log(response)
-    res.send({desc: "Video not found"})
-    throw new Error('Network response was not okay.');
-  })
-  .then(async (text) => {
-    let videoData = {}
-    const $ = cheerio.load(text)
-    const dataScript = $('script');
-    for(let i = 0; i < dataScript.length; i++) {
-      let data = dataScript.get(i).children[0] ? dataScript.get(i).children[0].data ? dataScript.get(i).children[0].data.includes('ytInitialData') ? dataScript.get(i).children[0].data : null : null : null;
-      if(data !== null && !data.includes('(function serverContract()'))  {
-      // console.log(text)
-      let cleanData = data.replace('var ytInitialData = ', '');
-      cleanData = cleanData.replace(';', '')
-      let jsonData = JSON.parse(cleanData)
-      // console.log(cleanData)
-      let overlayData = jsonData.overlay.reelPlayerOverlayRenderer
-      let views = jsonData.engagementPanels[1].engagementPanelSectionListRenderer.content.structuredDescriptionContentRenderer.items[0].videoDescriptionHeaderRenderer.views.simpleText
-      let channel = jsonData.engagementPanels[1].engagementPanelSectionListRenderer.content.structuredDescriptionContentRenderer.items[0].videoDescriptionHeaderRenderer.channel.simpleText
-      let publishDate = jsonData.engagementPanels[1].engagementPanelSectionListRenderer.content.structuredDescriptionContentRenderer.items[0].videoDescriptionHeaderRenderer.publishDate.simpleText
-      if(views) {
-        views = views.split(' ')[0]
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
       }
-      let likes = overlayData.likeButton.likeButtonRenderer.likeCount
-      let title = overlayData.reelPlayerHeaderSupportedRenderers.reelPlayerHeaderRenderer.reelTitleText.runs[0].text
-      let comments = overlayData.viewCommentsButton.buttonRenderer.accessibility.label
-      if(comments) {
-        comments = comments.split(' ')[0]
+      console.log(response);
+      res.send({ desc: "Video not found" });
+      throw new Error("Network response was not okay.");
+    })
+    .then(async (text) => {
+      let videoData = {};
+      const $ = cheerio.load(text);
+      const dataScript = $("script");
+      for (let i = 0; i < dataScript.length; i++) {
+        let data = dataScript.get(i).children[0]
+          ? dataScript.get(i).children[0].data
+            ? dataScript.get(i).children[0].data.includes("ytInitialData")
+              ? dataScript.get(i).children[0].data
+              : null
+            : null
+          : null;
+        if (data !== null && !data.includes("(function serverContract()")) {
+          // console.log(text)
+          let cleanData = data.replace("var ytInitialData = ", "");
+          cleanData = cleanData.replace(";", "");
+          let jsonData = JSON.parse(cleanData);
+          // console.log(cleanData)
+          let overlayData = jsonData.overlay.reelPlayerOverlayRenderer;
+          let views =
+            jsonData.engagementPanels[1].engagementPanelSectionListRenderer
+              .content.structuredDescriptionContentRenderer.items[0]
+              .videoDescriptionHeaderRenderer.views.simpleText;
+          let channel =
+            jsonData.engagementPanels[1].engagementPanelSectionListRenderer
+              .content.structuredDescriptionContentRenderer.items[0]
+              .videoDescriptionHeaderRenderer.channel.simpleText;
+          let publishDate =
+            jsonData.engagementPanels[1].engagementPanelSectionListRenderer
+              .content.structuredDescriptionContentRenderer.items[0]
+              .videoDescriptionHeaderRenderer.publishDate.simpleText;
+          if (views) {
+            views = views.split(" ")[0];
+          }
+          let likes = overlayData.likeButton.likeButtonRenderer.likeCount;
+          let title =
+            overlayData.reelPlayerHeaderSupportedRenderers
+              .reelPlayerHeaderRenderer.reelTitleText.runs[0].text;
+          let comments =
+            overlayData.viewCommentsButton.buttonRenderer.accessibility.label;
+          if (comments) {
+            comments = comments.split(" ")[0];
+          }
+          let channelUrl =
+            overlayData.reelPlayerHeaderSupportedRenderers
+              .reelPlayerHeaderRenderer.channelNavigationEndpoint
+              .commandMetadata.webCommandMetadata.url;
+          // let channel = overlayData.reelPlayerHeaderSupportedRenderers.reelPlayerHeaderRenderer.ownerText.runs[0].text
+          // let comments = overlayData.viewCommentsButton.buttonRenderer.text.simpleText
+          videoData = {
+            views,
+            channel,
+            publishDate,
+            likes,
+            title,
+            comments,
+            channelUrl,
+          };
+        }
       }
-      let channelUrl = overlayData.reelPlayerHeaderSupportedRenderers.reelPlayerHeaderRenderer.channelNavigationEndpoint.commandMetadata.webCommandMetadata.url
-      // let channel = overlayData.reelPlayerHeaderSupportedRenderers.reelPlayerHeaderRenderer.ownerText.runs[0].text
-      // let comments = overlayData.viewCommentsButton.buttonRenderer.text.simpleText
-      console.log('likes ', likes);
-      console.log('views ', views);
-      console.log('url ', url);
-      console.log('channel ', channel);
-      console.log('publish date ', publishDate);
-      console.log('title ', title);
-      console.log('comments ', comments);
-      console.log('channel url ', channelUrl);
-      // console.log(likes)
-      // let cleanData = data.replace('var ytInitialData = ', '');
-      // let scriptIndex = cleanData.indexOf('(function serverContract()');
-      // let jsonData = cleanData.slice(0, scriptIndex);
-      // console.log(scriptIndex)
-      // console.log(jsonData)
-      // jsonData = jsonData.replace(')()', '')
-      // jsonData = JSON.parse(jsonData)
-      // console.log(jsonData)
-      // let videoDetails = jsonData.contents.twoColumnWatchNextResults.results.results.contents[1].videoSecondaryInfoRenderer
-      // console.log(videoDetails)
-    }
-    }
-    // const dataScript = $('#SIGI_STATE').text();
-    // const jsonData = JSON.parse(dataScript);
-    // const ItemModule = jsonData.ItemModule
-    // const keys = Object.keys(ItemModule);
-    // let key = keys[0];
-    // const dataObject = ItemModule[`${key}`]
-    // console.log(dataObject)
-    // let videoUrl = `https://www.tiktok.com/@${dataObject.author}/video/${dataObject.id}`
-    // let createTime = dataObject.createTime;
-    // let scheduleTime = dataObject.scheduleTime;
-    // let finalTime;
-    // if (scheduleTime !== 0) {
-    //   finalTime = new Date(scheduleTime * 1000);
-    // }
-    // else {
-    //   finalTime = new Date(createTime * 1000);
-    // }
-    // videoData.createTime = `${finalTime.getDate()}-${finalTime.getMonth() + 1}-${finalTime.getFullYear()}`
-    // videoData.videoUrl =  videoUrl
-    // videoData.author = (dataObject.author).trim();
-    // videoData.desc = dataObject.desc
-    // videoData.durations = dataObject.video.duration
-    // videoData = {...videoData, ...dataObject.stats}
-    // console.log(videoData)
-    return videoData
-
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+      return videoData;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
   res.send(await data);
-})
+});
 
-
-app.post('/tiktok', async (req, res) => {
+app.post("/tiktok", async (req, res) => {
   const { url } = req.query;
-  console.log(url)
+  console.log(url);
   let data = fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.text();
-    }
-    console.log(response)
-    res.send({desc: "Video not found"})
-    throw new Error('Network response was not okay.');
-  })
-  .then(async (text) => {
-    let videoData = {}
-    const $ = cheerio.load(text)
-    const dataScript = $('#SIGI_STATE').text();
-    const jsonData = JSON.parse(dataScript);
-    const ItemModule = jsonData.ItemModule
-    const keys = Object.keys(ItemModule);
-    let key = keys[0];
-    const dataObject = ItemModule[`${key}`]
-    console.log(dataObject)
-    let videoUrl = `https://www.tiktok.com/@${dataObject.author}/video/${dataObject.id}`
-    let createTime = dataObject.createTime;
-    let scheduleTime = dataObject.scheduleTime;
-    let finalTime;
-    if (scheduleTime !== 0) {
-      finalTime = new Date(scheduleTime * 1000);
-    }
-    else {
-      finalTime = new Date(createTime * 1000);
-    }
-    videoData.createTime = `${finalTime.getDate()}-${finalTime.getMonth() + 1}-${finalTime.getFullYear()}`
-    videoData.videoUrl =  videoUrl
-    videoData.author = (dataObject.author).trim();
-    videoData.desc = dataObject.desc
-    videoData.durations = dataObject.video.duration
-    videoData = {...videoData, ...dataObject.stats}
-    console.log(videoData)
-    return videoData
-
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      console.log(response);
+      res.send({ desc: "Video not found" });
+      throw new Error("Network response was not okay.");
+    })
+    .then(async (text) => {
+      let videoData = {};
+      const $ = cheerio.load(text);
+      const dataScript = $("#SIGI_STATE").text();
+      const jsonData = JSON.parse(dataScript);
+      const ItemModule = jsonData.ItemModule;
+      const keys = Object.keys(ItemModule);
+      let key = keys[0];
+      const dataObject = ItemModule[`${key}`];
+      console.log(dataObject);
+      let videoUrl = `https://www.tiktok.com/@${dataObject.author}/video/${dataObject.id}`;
+      let createTime = dataObject.createTime;
+      let scheduleTime = dataObject.scheduleTime;
+      let finalTime;
+      if (scheduleTime !== 0) {
+        finalTime = new Date(scheduleTime * 1000);
+      } else {
+        finalTime = new Date(createTime * 1000);
+      }
+      videoData.createTime = `${finalTime.getDate()}-${
+        finalTime.getMonth() + 1
+      }-${finalTime.getFullYear()}`;
+      videoData.videoUrl = videoUrl;
+      videoData.author = dataObject.author.trim();
+      videoData.desc = dataObject.desc;
+      videoData.durations = dataObject.video.duration;
+      videoData = { ...videoData, ...dataObject.stats };
+      console.log(videoData);
+      return videoData;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 
   res.send(await data);
 });
